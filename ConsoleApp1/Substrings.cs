f9f606f53c8e6aa9607c4c945bb750f1471e74fa
Emma Gunnari <emma.gunnari@iths.se>
2021-03-24 14:46:01 +0100

    Add project files.

---
+++ ConsoleApp1/Substrings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Numerics;
+
+namespace Substrings
+{
+    class Substrings
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string with letters and digits: "); //Input string
+            string setString = Console.ReadLine();
+            BigInteger sum = 0; //BigInteger because the sum might get big
+
+            for (int i = 0; i < setString.Length - 1; i++)
+            {
+                if (!int.TryParse(setString[i].ToString(), out _)) 
+                {
+                    continue;
+                }
+
+                for (int a = i + 1; a < setString.Length; a++)
+                {
+                    if (!int.TryParse(setString[a].ToString(), out _))
+                    {
+                        break;
+                    }
+                    if (setString[i] == setString[a])
+                    {
+                       
+                        string digit = "";
+                        for (int j = i; j <= a; j++)
+                        {
+                            digit += setString[j];
+                        }
+                        
+                        sum += long.Parse(digit); //Calculates the substrings
+
+                        for (var s = 0; s < setString.Length; s++)
+                        {
+                            //Print the marked substrings in color Magenta & the others in white
+                            Console.ForegroundColor = s >= i && s <= a ? ConsoleColor.Magenta : ConsoleColor.White; 
+                            Console.Write(setString[s]);
+                            Console.ResetColor(); //Reset colors
+                        }
+                        //Puts every string on a new line
+                        Console.WriteLine(); 
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(" "); //Just a blank line
+            Console.WriteLine("Total sum of substrings: " + sum.ToString()); //Output the total sum of substrings
+        }
+    }
+}
---
+++ Search+sum/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Labb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string");
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray(); // Dela upp strängen i en char array för enkel genomsökning
+            List<Substring> substringList = new List<Substring>(); // Lista att stoppa in alla lyckade delsträngar 
+                                                                  // (struct Substring) och deras respektive StartIndex
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                // Om charArray[i] är en siffra så börjar vi söka igenom hela arrayen efter samma siffra
+                if (char.IsDigit(charArray[i]) )
+                {
+                    for (int j = i + 1; j < charArray.Length; j++) 
+                    { 
+                        if (charArray[i] == charArray[j]) // Om samma siffra kunde hittas..
+                        {
+                            // så skapar vi en struct Substring av input-strängen som börjar och slutar på samma siffra och lägger till den i vår lista
+                            substringList.Add(new Substring(text: input.Substring(i, j - i + 1), startIndex: i));
+                            break; // Bryt ut ur inner-loopen
+                        }
+                        else if (!char.IsDigit(charArray[j]))
+                        {
+                            break; // Bryt ut ur inner-loopen om vi hittar en icke-siffra
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < substringList.Count; i++)
+            {
+                for (int charIndex = 0; charIndex < charArray.Length; charIndex++)
+                {
+                    if (substringList[i].StartIndex != charIndex)
+                    {
+                        // Alla tecken som inte är markerade skrivs ut i blått
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(charArray[charIndex]);
+                    }
+                    else
+                    {
+                        // De markerade delsträngarna skrivs ut i rött
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(substringList[i].Text);
+                        charIndex += substringList[i].Text.Length - 1;
+                    }
+                }
+                Console.WriteLine();
+            }
+            BigInteger sum = 0; // BigInteger eftersom summan kan bli väldigt stor
+            foreach (var substr in substringList)
+            {
+                sum += BigInteger.Parse(substr.Text);
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+            Console.WriteLine("Total = {0}", sum);
+        }
+    }
+    public struct Substring
+    {
+        public string Text;
+        public int StartIndex;
+        public Substring(string text, int startIndex)
+        {
+            Text = text;
+            StartIndex = startIndex;
+        }
+    }
+}
